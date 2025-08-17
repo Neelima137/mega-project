5cc828bf297470a61d76326bc49e4305114aa727
Neelima137 <devadineelima137@gmail.com>
2025-08-17 12:54:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ resource "aws_eks_node_group" "project" {
     min_size     = 3
   }
 
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.small"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
